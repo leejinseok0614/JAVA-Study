19e7c8cedbf1366e4a2936de69aee07393a92de9
이진석 (LeeJinSeok) <87289223+leejinseok0614@users.noreply.github.com>
2023-08-18 18:10:04 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.14 ms, Memory: 70.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        while(n>0) {
+            answer += n%10;
+            n /= 10;
+        }
+        
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
