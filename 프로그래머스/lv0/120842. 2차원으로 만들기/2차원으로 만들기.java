42a6b13dbca4758a0b4ad3a2daaf056a5341b2eb
이진석 (LeeJinSeok) <87289223+leejinseok0614@users.noreply.github.com>
2023-08-31 16:29:55 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.02 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[][] solution(int[] num_list, int n) {
+        int[][] answer = new int[num_list.length / n][n];
+        int length = 0;
+        
+        for(int i=0; i<num_list.length / n; i++) {
+            for(int j=0; j<n; j++) {
+                answer[i][j] = num_list[length++];
+            }
+        }
+        return answer;
+    }
+}
