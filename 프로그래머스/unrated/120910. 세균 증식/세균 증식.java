a19c7b23297f4714d936251e0e75fca2a5822219
이진석 (LeeJinSeok) <87289223+leejinseok0614@users.noreply.github.com>
2023-10-20 16:29:56 +0900

    [level unrated] Title: 세균 증식, Time: 0.01 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n, int t) {
+        int answer = 0;
+        
+        for(int i=1; i<=t; i++) {
+            answer = n *= 2;
+        }
+        return answer;
+    }
+}
